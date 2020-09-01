fb3dab10d3dd782a6c43bb2dc482fbac7e8856df
Daniel Guerra <danielguerra.g12@gmail.com>
2020-08-31 21:27:43 -0300

    Criei as pastas de model e as classes com atributos de paginação.

---
+++ src/app/shared/model/responsePageable.model.ts
@@ -0,0 +1,11 @@
+export class ResponsePageable {
+    content: any[];
+    last: boolean;
+    number: number;
+    numberOfElements: number;
+    pageable: any[];
+    size: number;
+    sort: number;
+    totalElements: number;
+    totalPages: number;
+}
